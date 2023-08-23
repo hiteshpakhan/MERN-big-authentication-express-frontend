a1ffd5bf1b7d153c64db69eb6fe40a60ede30c41
Hitesh Pakhan <77678075+hiteshpakhan@users.noreply.github.com>
2023-08-23 17:16:12 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
+    <>
     <div className='flex justify-center items-center gap-10 font-bold text-lg shadow-md py-3 text-white' style={{backgroundColor: "#5C5470"}}>
         <Link to="/" >Home</Link>
 
         <Link to="/createblog" >Create Blog</Link>
     </div>
+    <div className='flex justify-center items-center gap-10 font-bold text-lg shadow-md py-1 text-white' >Created By Hitesh Pakhan this is FullStack MERN application use it</div>
+    </>
   )
 }
 
