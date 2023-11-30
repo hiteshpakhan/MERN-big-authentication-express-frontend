f4e1ee17b2376a067a345f573bf6488716a8fb99
Hitesh Pakhan <77678075+hiteshpakhan@users.noreply.github.com>
2023-11-30 09:26:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='flex justify-center items-center gap-10 font-bold text-lg shadow-md py-3 text-white' style={{backgroundColor: "#5C5470"}}>
         <Link to="/" >Home</Link>
 
-        <Link to="/createblog" >Create Blog</Link>
+        <Link to="/createblog" >Create Task</Link>
     </div>
     <div className='flex justify-center items-center gap-10 font-bold text-lg shadow-md py-1 text-white' >Created By Hitesh Pakhan this is FullStack MERN application use it</div>
     </>
